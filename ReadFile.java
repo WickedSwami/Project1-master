2cf5b1044f09ec5b1bd377e99f951e30fba5a32e
Eric <stupparde@xavier.edu>
2015-09-10 12:40:26 -0700

    some
    changes

--- ReadFile.java
+++ ReadFile.java
@@ -27,9 +27,11 @@ public class ReadFile
         while (infile.hasNext())
         {
             String word = infile.next();
-            ArrayList<String> wordBank = new ArrayList<String>();
-            wordBank.add(word);
+            countWords.put(word,0);
             
+            
+            /*ArrayList<String> wordBank = new ArrayList<String>();
+            wordBank.add(word);
             countWords.put(word,1);
 
             for (String w : wordBank) {
@@ -41,7 +43,10 @@ public class ReadFile
                     consecutiveOcc++;
                     countWords.put(w,new Integer(consecutiveOcc));
                 }
-            }
+            }  */
+            
+            //int[] valueList = new int[countWords.size()];
+            //mergeSort(valueList);
             System.out.println(word);
         }
         
@@ -53,15 +58,19 @@ public class ReadFile
     * @param outputFile is the file object for output
     * @param someWords is a list of words to print out
     **/
-    public void writeIt(PrintWriter outputFile, ArrayList<String> someWords)
+    public void writeIt(PrintWriter outputFile, HashMap<String,Integer> someWords)
     {
         outputFile.println("<html>");
         outputFile.println("<body>");
         
-        for (String word: someWords) {
-            int freq = countWords.get(word);
+        this.countWordOccurences(someWords);
+        
+        /*for (String word: someWords.keySet()) {
+            
+            int freq = someWords.get(word);
             outputFile.println("<p style='font-size:"+freq*10+"'>"+word+"</p>");
-        }
+        }*/
+        outputFile.println(someWords);
         
         outputFile.println("</html>");
         outputFile.println("</body>");
@@ -69,10 +78,33 @@ public class ReadFile
  
     public HashMap<String,Integer> getCountWords()
     {
+        /*ArrayList<String> allWords = new ArrayList<String>();
+        
+        for (String word : countWords.keySet()) {
+            allWords.add(word);
+        }
+        
+        return allWords;
+        */
+       
         return countWords;
     }
     
-    
+    public HashMap<String, Integer> countWordOccurences(HashMap<String,Integer> words) {
+        
+        for (String word : words.keySet()) {
+            if (countWords.containsKey(word)) {
+                Integer value = 1;
+                countWords.put(word, new Integer(value));
+            } else {
+                Integer consecutiveOcc = countWords.get(word);
+                consecutiveOcc ++;
+                countWords.put(word, new Integer(consecutiveOcc));
+            }
+        }
+        
+        return countWords;
+    }
 
     public static void main(String [] args)
     {
@@ -85,16 +117,10 @@ public class ReadFile
             try {
                 Scanner in = new Scanner(fileToRead);
                 mainObject.readIt(in);
-                /* Now demonstrate a PrintWriter for printing a file */
+                HashMap<String,Integer> wordCloud = mainObject.getCountWords();
+                mainObject.countWordOccurences(wordCloud);
+
                 PrintWriter outFile = new PrintWriter("WickedWords.html");
-                HashMap<String,Integer> myWords = mainObject.getCountWords();
-                ArrayList<String> wordCloud = new ArrayList<String>();
-                
-                for (String word : myWords.keySet()) {
-                    int freq = myWords.get(word);
-                    wordCloud.add(word);
-                }
-                
                 mainObject.writeIt(outFile,wordCloud);
                 outFile.close();
             } catch (IOException e) {
@@ -103,6 +129,54 @@ public class ReadFile
         }
     
     }
+    
+    /*public static void mergeSort(int[] theList)
+    {
+        mergeSortWork(theList,0,theList.length-1);
+    }
+    
+    public static void mergeSortWork(int [] theList, int startIndex, int endIndex)
+    {
+    	if (startIndex < endIndex) {
+    		int listLength = endIndex - startIndex+1;
+    		int middle = startIndex + listLength/2;
+    		mergeSortWork(theList, startIndex, middle);
+    		mergeSortWork(theList, middle+1, endIndex);
+    		merge(theList, startIndex, middle+1, endIndex);
+    	}
+    }
+    
+    public static void merge(int [] theList, int startA, int startB, int endB) 
+    {
+    	int listLength = endB-startA + 1;
+    	int[] temporaryList = new int[listLength];
+    	int index = 0;
+    	int indexA = startA;
+    	int indexB = startB;
+    	while (indexA < startB && indexB <= endB) {
+    		if (theList[indexA] < theList[indexB]) {
+    			temporaryList[index] = theList[indexA];
+    			indexA++;
+    		} else {
+    			temporaryList[index] = theList[indexB];
+    			indexB++;
+    		}
+    		
+    		index++;
+    	}
+    	
+    	for (;indexA<startB;indexA++,index++) {
+    		temporaryList[index] = theList[indexA];
+    	}
+    	
+    	for (;indexB<=startB;indexB++,index++) {
+    		temporaryList[index] = theList[indexB];
+    	}
+    	
+    	for (int i = 0; i < index; i++) {
+    		theList[startA+i]=temporaryList[i];
+    	}
+    }*/
     
     
 }
